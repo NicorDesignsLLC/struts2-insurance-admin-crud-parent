689e5a36638c2cd83a566711f35f57d50d3f4316
Nicolaas Kock <nicocpk@gmail.com>
2019-10-21 07:16:42 -0400

    Updating SQL Scripts

---
+++ src/sqlscripts/create-app-user.sql
@@ -0,0 +1,4 @@
+CREATE USER 'insurance_app'@'localhost' IDENTIFIED BY 'insurance_app';
+
+GRANT ALL PRIVILEGES ON *.* TO 'insurance_app'@'localhost';
+
---
+++ src/sqlscripts/create-insurance-admin-db.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS insurance_admin;
+CREATE DATABASE insurance_admin;
